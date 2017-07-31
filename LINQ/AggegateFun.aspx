fdbb2e268622abff88611cbf4699885926e0488d
debugdk <deepakumarajput@outlook.com>
2017-07-31 18:44:46 +0530

    efs

---
+++ LINQ/AggegateFun.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="AggegateFun.aspx.cs" Inherits="LINQ.AggegateFun" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+   adsafsaf
+        
+    </div>
+    </form>
+</body>
+</html>
---
+++ LINQ/AggegateFun.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LINQ
+{
+    public partial class AggegateFun : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int[] number = { 2, 3, 4, 5 };
+            int result = number.Aggregate((a, b) => a * b);
+            Response.Write(result);
+        }
+
+        
+    }
+}
